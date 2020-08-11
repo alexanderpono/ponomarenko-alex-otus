a522ce4c71e60f18cd130fbe2be98f2b8ee3a224
Alexander Ponomarenko <aponomarenko@mtt.ru>
2020-08-12 01:17:29 +0300

    [node-4]	added eslint, babel, typescript, jest

---
+++ node/node-hw4/src/sum1.js
@@ -0,0 +1,5 @@
+function sum1(a, b) {
+    return a + b;
+}
+
+module.exports = { sum1 };
---
+++ node/node-hw4/src/sum2.test.ts
@@ -0,0 +1,4 @@
+import { sum2 } from './sum2';
+test('sum2: 2 + 4 = 6', () => {
+    expect(sum2(2, 4)).toBe(6);
+});
