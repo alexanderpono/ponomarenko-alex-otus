c24a1be74667b29f5224ee0727a57fb60beac9d6
Alexander Ponomarenko <brain16383@yandex.ru>
2021-01-04 14:38:09 +0300

    [node-4] add express, routing-controllers and a dummy controller

--- node/node-hw4/src/index.ts
+++ node/node-hw4/src/index.ts
@@ -1,1 +1,11 @@
-console.log('index.ts2!');
+console.log('index.ts!');
+
+import { createExpressServer } from 'routing-controllers';
+import { UserController } from './controllers/UserController';
+import 'reflect-metadata';
+
+const app = createExpressServer({
+    controllers: [UserController]
+});
+
+app.listen(3000);
