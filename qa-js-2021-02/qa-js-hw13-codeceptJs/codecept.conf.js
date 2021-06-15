d6e68b56af4fd921795bae0e82b392b7ae8b034e
Alexander Ponomarenko <brain16383@yandex.ru>
2021-06-16 01:26:54 +0300

    [qa-js-hw13] Added codeceptjs framework

---
+++ qa-js-2021-02/qa-js-hw13-codeceptJs/codecept.conf.js
@@ -0,0 +1,35 @@
+const { setHeadlessWhen } = require('@codeceptjs/configure');
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+    Playwright: {
+      url: 'http://demo.testfire.net/',
+      show: true,
+      browser: 'chromium'
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'qa-js-hw13-codeceptJs',
+  plugins: {
+    pauseOnFail: {},
+    retryFailedStep: {
+      enabled: true
+    },
+    tryTo: {
+      enabled: true
+    },
+    screenshotOnFail: {
+      enabled: true
+    }
+  }
+}
