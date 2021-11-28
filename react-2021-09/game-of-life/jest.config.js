e8bba5a4109e9e92dbb656c315f415417ad722d0
Alexander Ponomarenko <brain16383@yandex.ru>
2021-11-28 18:47:34 +0300

    [react-hw4] CI-18 added deploy test-report to GHP

--- react-2021-09/game-of-life/jest.config.js
+++ react-2021-09/game-of-life/jest.config.js
@@ -7,6 +7,16 @@ module.exports = {
     collectCoverage: true,
     collectCoverageFrom: ['src/**/*.(ts|tsx)', '!src/**/*.stories.tsx', '!src/app.tsx', '!src/**/index.tsx'],
     testEnvironment: 'jsdom',
+    reporters: [
+        'default',
+        [
+            './node_modules/jest-html-reporter',
+            {
+                pageTitle: 'Test Report',
+                outputPath: './temp/testResult.html',
+            },
+        ],
+    ],
     setupFilesAfterEnv: ['<rootDir>/internals/jestSettings.js'],
     transform: {
         '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
