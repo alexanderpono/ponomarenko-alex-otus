afc47231b87debf41a3382c19a4ef5df39f8fe31
Alexander Ponomarenko <brain16383@yandex.ru>
2021-12-26 17:20:29 +0300

    [react-hw8] Fix storybook

--- react-2021-09/game-of-life/src/components/UserNameForm/UserNameForm.stories.tsx
+++ react-2021-09/game-of-life/src/components/UserNameForm/UserNameForm.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { UserNameForm } from './UserNameForm';
 import { action } from '@storybook/addon-actions';
 import { withKnobs, number, boolean, text } from '@storybook/addon-knobs';
+import { BrowserRouter } from 'react-router-dom';
 
 export default {
     title: 'UserNameForm',
@@ -10,11 +11,21 @@ export default {
 };
 
 export const Static = () => {
-    return <UserNameForm onName={action('name')} userName="" onLogout={action('onLogout')} />;
+    return (
+        <BrowserRouter>
+            <UserNameForm onName={action('name')} userName="" onLogout={action('onLogout')} />
+        </BrowserRouter>
+    );
 };
 
 export const StaticLoggedIn = () => {
     return (
-        <UserNameForm onName={action('name')} userName="user123" onLogout={action('onLogout')} />
+        <BrowserRouter>
+            <UserNameForm
+                onName={action('name')}
+                userName="user123"
+                onLogout={action('onLogout')}
+            />
+        </BrowserRouter>
     );
 };
