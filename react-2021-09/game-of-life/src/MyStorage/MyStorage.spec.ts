52d348ecb41ef5ec7335e613b3068267e54cfcbb
Alexander Ponomarenko <brain16383@yandex.ru>
2022-01-11 01:02:33 +0300

    [react-hw9] Fixes-1 after code review

--- react-2021-09/game-of-life/src/MyStorage/MyStorage.spec.ts
+++ react-2021-09/game-of-life/src/MyStorage/MyStorage.spec.ts
@@ -3,34 +3,15 @@ import { str } from '@src/testFramework/lib/reducer';
 import { MyStorage } from './MyStorage';
 
 describe('MyStorage', () => {
-    // by Holben888@github from https://bholmes.dev/blog/mocking-browser-apis-fetch-localstorage-dates-the-easy-way-with-jest/
-    // let's make a mock fridge (storage) for all our tests to use
-    let mockFridge: Record<string, string> = {};
-
-    beforeAll(() => {
-        global.Storage.prototype.setItem = jest.fn((key, value) => {
-            mockFridge[key] = value;
-        });
-        global.Storage.prototype.getItem = jest.fn((key) => mockFridge[key]);
-    });
-
     beforeEach(() => {
-        // make sure the fridge starts out empty for each test
-        mockFridge = {};
-    });
-
-    afterAll(() => {
-        // return our mocks to their original values
-        // 🚨 THIS IS VERY IMPORTANT to avoid polluting future tests!
-        (global.Storage.prototype.setItem as jest.Mock).mockReset();
-        (global.Storage.prototype.getItem as jest.Mock).mockReset();
+        localStorage.clear();
     });
 
     test('.setState(state) writes state to localStorage.state', () => {
         const myStorage = new MyStorage();
         const rndState = { ...defaultAppState, userName: str() };
         myStorage.setState(rndState);
-        expect(JSON.parse(mockFridge['state'])).toEqual(rndState);
+        expect(JSON.parse(localStorage['state'])).toEqual(rndState);
     });
 
     describe('.getState', () => {
@@ -42,13 +23,13 @@ describe('MyStorage', () => {
         it('returns localStorage.name if localStorage.state != ""', () => {
             const myStorage = new MyStorage();
             const rndState = { ...defaultAppState, userName: str() };
-            mockFridge['state'] = JSON.stringify(rndState);
+            localStorage['state'] = JSON.stringify(rndState);
             expect(myStorage.getState()).toEqual(rndState);
         });
 
         it('returns null if JSON.parse(localStorage.state) throws', () => {
             const myStorage = new MyStorage();
-            mockFridge['state'] = 'abc';
+            localStorage['state'] = 'abc';
             expect(myStorage.getState()).toBe(null);
         });
     });
@@ -62,7 +43,7 @@ describe('MyStorage', () => {
         it('resolves if state is not empty', () => {
             const myStorage = new MyStorage();
             const rndState = { ...defaultAppState, userName: str() };
-            mockFridge['state'] = JSON.stringify(rndState);
+            localStorage['state'] = JSON.stringify(rndState);
             return expect(myStorage.loadState()).resolves.toEqual(rndState);
         });
     });
