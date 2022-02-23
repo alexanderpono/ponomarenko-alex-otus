b86a08bc4fb0a0de8932408b59e975fe3772a458
Alexander Ponomarenko <brain16383@yandex.ru>
2022-02-23 15:17:10 +0300

    [react-hw13] Fixed issue #45 - simplify checks of mocks

--- react-2021-09/game-of-life/src/components/SetFillPercent/SetFillPercent.spec.tsx
+++ react-2021-09/game-of-life/src/components/SetFillPercent/SetFillPercent.spec.tsx
@@ -4,39 +4,18 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { FillPercent } from '@src/consts';
 
-describe('FlllPercent', () => {
-    it('calls fill() callback when 0% is clicked', () => {
+describe('FlllPercent - parameterized tests', () => {
+    test.each`
+        curPercent          | clickAt
+        ${FillPercent.P25}  | ${'0%'}
+        ${FillPercent.P50}  | ${'25%'}
+        ${FillPercent.P75}  | ${'50%'}
+        ${FillPercent.P100} | ${'75%'}
+        ${FillPercent.P25}  | ${'100%'}
+    `('calls fill() callback when $clickAt is clicked', ({ clickAt, sizeId }) => {
         const mockCallBack = jest.fn();
         render(<SetFillPercent fill={mockCallBack} curPercent={FillPercent.P25} />);
         userEvent.click(screen.getByText('clear'));
-        expect(mockCallBack.mock.calls.length).toEqual(1);
-    });
-
-    it('calls fill() callback when 25% is clicked', () => {
-        const mockCallBack = jest.fn();
-        render(<SetFillPercent fill={mockCallBack} curPercent={FillPercent.P50} />);
-        userEvent.click(screen.getByText('25%'));
-        expect(mockCallBack.mock.calls.length).toEqual(1);
-    });
-
-    it('calls fill() callback when 50% is clicked', () => {
-        const mockCallBack = jest.fn();
-        render(<SetFillPercent fill={mockCallBack} curPercent={FillPercent.P75} />);
-        userEvent.click(screen.getByText('50%'));
-        expect(mockCallBack.mock.calls.length).toEqual(1);
-    });
-
-    it('calls fill() callback when 75% is clicked', () => {
-        const mockCallBack = jest.fn();
-        render(<SetFillPercent fill={mockCallBack} curPercent={FillPercent.P100} />);
-        userEvent.click(screen.getByText('75%'));
-        expect(mockCallBack.mock.calls.length).toEqual(1);
-    });
-
-    it('calls fill() callback when 100% is clicked', () => {
-        const mockCallBack = jest.fn();
-        render(<SetFillPercent fill={mockCallBack} curPercent={FillPercent.P25} />);
-        userEvent.click(screen.getByText('100%'));
-        expect(mockCallBack.mock.calls.length).toEqual(1);
+        expect(mockCallBack).toBeCalledTimes(1);
     });
 });
