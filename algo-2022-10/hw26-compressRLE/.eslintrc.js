ffbc3c8715b9906d8e4f02d69b86da058488815f
Alexander Ponomarenko <brain16383@yandex.ru>
2023-02-02 23:44:12 +0300

    [algo-2022-10/hw26] Add project template

---
+++ algo-2022-10/hw26-compressRLE/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true
+    },
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly'
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module'
+    },
+    plugins: ['@typescript-eslint', 'prettier'],
+    rules: {
+        semi: ['warn', 'always'],
+        'prettier/prettier': [
+            'error',
+            {
+                endOfLine: 'auto'
+            }
+        ],
+        '@typescript-eslint/explicit-function-return-type': 0,
+        'react/prop-types': 0
+    }
+};
