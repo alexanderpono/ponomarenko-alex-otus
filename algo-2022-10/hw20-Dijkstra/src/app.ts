95929779d0840ccb3522cec435aeccb7fe84997c
Alexander Ponomarenko <brain16383@yandex.ru>
2023-02-23 19:05:35 +0300

    [algo-2022-10/hw20] Add realization of Dijkstra algorithm

--- algo-2022-10/hw20-Dijkstra/src/app.ts
+++ algo-2022-10/hw20-Dijkstra/src/app.ts
@@ -1,1 +1,47 @@
-console.log('app!');
+import { Graph, SILENT, VERBOSE } from './Graph';
+
+const adjacencyMatrix = `
+  AABBCCDDEEFFGG
+AA..020306......
+BB02..04..09....
+CC0304..010706..
+DD06..01....04..
+EE..0907....0105
+FF....060401..08
+GG........0508..
+`;
+console.log('adjacencyMatrix=', adjacencyMatrix);
+console.log(
+    '\nadjacencyMatrix=',
+    JSON.stringify(Graph.create().initFromAdjacencyString(adjacencyMatrix).getMatrix())
+);
+
+console.log(Graph.create().initFromAdjacencyString(adjacencyMatrix).calcEdges().printEdges());
+
+console.log(
+    Graph.create()
+        .initFromAdjacencyString(adjacencyMatrix)
+        .calcEdges()
+        .calcVertices()
+        .printVertices('vertices after init')
+);
+
+console.log(
+    Graph.create()
+        .initFromAdjacencyString(adjacencyMatrix)
+        .calcEdges()
+        .calcVertices()
+        .calcVerticesCost(6, 0, VERBOSE)
+        .calcCheapestPath(6, 0)
+        .printEdgesInCheapestPath(6, 0)
+);
+
+console.log(
+    Graph.create()
+        .initFromAdjacencyString(adjacencyMatrix)
+        .calcEdges()
+        .calcVertices()
+        .calcVerticesCost(6, 0, SILENT)
+        .calcCheapestPath(6, 0)
+        .printPathFromTo(6, 0)
+);
