fd6750219b844df51423d8482c0cbd07ade3227a
Alexander Ponomarenko <brain16383@yandex.ru>
2023-07-24 01:13:58 +0300

    [dijkstra-inno-beer-2023-08]: Added demo project + CI/CD

---
+++ algo-2022-10/dijkstra-inno-beer-2023-08/demo/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = (env, argv) => {
+    console.log('argv=', argv);
+    const plugins =
+            [
+                  new CopyPlugin({
+                      patterns: [
+                          {
+                              from: 'app-static/sprite.png',
+                              to: path.join(__dirname, '/demo-dist/sprite.png')
+                          },
+                          {
+                              from: 'bundled/index.html',
+                              to: path.join(__dirname, '/demo-dist/index.html')
+                          },
+                          { from: 'bundled/shower', to: path.join(__dirname, '/demo-dist/shower') },
+                          { from: 'bundled/demo-static', to: path.join(__dirname, '/demo-dist/demo-static') },
+                          { from: 'bundled/app-static', to: path.join(__dirname, '/demo-dist/app-static') }
+                      ]
+                  })
+              ];
+
+    const config = {
+        entry: './src/index.js',
+        devtool: 'source-map',
+        resolve: {
+            extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+            alias: {
+                types: path.resolve(__dirname, 'src/types')
+            }
+        },
+        output: {
+            path: path.join(__dirname, '/temp'),
+            filename: 'index.js'
+        },
+        module: {
+            rules: []
+        },
+        plugins
+    };
+
+    return config;
+};
+
