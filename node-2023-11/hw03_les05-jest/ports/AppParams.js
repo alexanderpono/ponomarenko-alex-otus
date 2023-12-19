18318a60f5c6e6f6b369d8e8321dd0250fed9484
Alexander Ponomarenko <brain16383@yandex.ru>
2023-12-19 23:14:35 +0300

    [hw03_les05-jest] copyfrom project hw01-tree

---
+++ node-2023-11/hw03_les05-jest/ports/AppParams.js
@@ -0,0 +1,39 @@
+class AppParams {
+    logger = null;
+    appProcess = null;
+
+    constructor(logger, appProcess) {
+        this.logger = logger;
+        this.appProcess = appProcess;
+    }
+
+    validateParams = (params) => {
+        if (params.length < 3) {
+            this.logger.log('\nFATAL: not enough parameters');
+            this.logger.printUsage();
+            this.appProcess.exit();
+        }
+        
+        if (params[1] != '-d') {
+            this.logger.log('\nFATAL: parameter [1] must be = -d');
+            this.logger.printUsage();
+            this.appProcess.exit();
+        }
+        
+        if (isNaN(parseInt(params[2]))) {
+            this.logger.log('\nFATAL: parameter [2] must be a number');
+            this.logger.printUsage();
+            this.appProcess.exit();
+        }
+    }
+
+    parseParams = (params) => ({
+        dirName: params[0],
+        depth: parseInt(params[2])
+    });
+    
+}
+
+module.exports = {
+    AppParams
+}
---
+++ node-2023-11/hw03_les05-jest/tree.js
@@ -0,0 +1,27 @@
+const { FsInput } = require('./ports/FsInput');
+const { AppParams } = require('./ports/AppParams');
+const { Logger } = require('./ports/Logger');
+const { AppProcess } = require('./ports/AppProcess');
+const { Graph, dir } = require('./Graph');
+
+const logger = new Logger();
+const appParams = new AppParams(logger, new AppProcess());
+const rawParams = process.argv.slice(2);
+
+logger.printAbout();
+appParams.validateParams(rawParams);
+
+const params = appParams.parseParams(rawParams);
+new FsInput().getDirStats(params.dirName, '', [], params.depth).then((foundFiles) => {
+    const sorted = foundFiles.sort((a, b) => {
+        if (a.name === b.name) {
+            return 0;
+        }
+        return a.name < b.name ? -1 : 1;
+    });
+
+    const treeLines = new Graph().draw(sorted);
+
+    console.log(dir(params.dirName));
+    console.log(treeLines.join('\n'));
+});
