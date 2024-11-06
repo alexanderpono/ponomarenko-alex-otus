d098ef5205cbd9293ef7ebb98910714e74693fae
alexanderpono <brain16383@yandex.ru>
2024-11-06 13:06:11 +0300

    [linux-basic-projectWork] Added fixFolders.sh script

---
+++ linux-basic-2024-07/linux-projectWork/fixFolders.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir ./prometheus/data
+sudo chown 65534:65534 ./prometheus/data
+
+mkdir ./logstash/data
+sudo chmod 777 ./logstash/data
+
+mkdir ./kibana/data
+sudo chown 65534:65534 ./kibana/data 
+sudo chmod 777 ./kibana/data
+
+mkdir ./project-api-balancer-filebeat/data
+sudo chmod 777 ./project-api-balancer-filebeat/data
+
+mkdir ./es
+mkdir ./es/data
+sudo chmod 777 ./es/data
