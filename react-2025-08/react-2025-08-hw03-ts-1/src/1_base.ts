14bd45d538f50648517a861d4b01db0048790819
alexanderpono <brain16383@yandex.ru>
2025-10-13 22:34:57 +0300

    fix: react-2025-08-hw03-ts-1 - fixes after code review

--- react-2025-08/react-2025-08-hw03-ts-1/src/1_base.ts
+++ react-2025-08/react-2025-08-hw03-ts-1/src/1_base.ts
@@ -1,16 +1,16 @@
 /**
  * Нужно превратить файл в ts и указать типы аргументов и типы возвращаемого значения
  * */
-export const removePlus = (s: string) => s.replace(/^\+/, '');
+export const removePlus = (s: string): string => s.replace(/^\+/, '');
 
-export const addPlus = (s: string) => `+${s}`;
+export const addPlus = (s: string): string => `+${s}`;
 
-export const removeFirstZeros = (value: string) => value.replace(/^(-)?[0]+(-?\d+.*)$/, '$1$2');
+export const removeFirstZeros = (value: string): string => value.replace(/^(-)?[0]+(-?\d+.*)$/, '$1$2');
 
-export const getBeautifulNumber = (value: number, separator = ' ') =>
+export const getBeautifulNumber = (value: number, separator = ' '): string =>
   value?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, separator);
 
-export const round = (value: number, accuracy = 2) => {
+export const round = (value: number, accuracy = 2): number => {
   const d = 10 ** accuracy;
   return Math.round(value * d) / d;
 };
@@ -18,7 +18,12 @@ export const round = (value: number, accuracy = 2) => {
 const transformRegexp =
   /(matrix\(-?\d+(\.\d+)?, -?\d+(\.\d+)?, -?\d+(\.\d+)?, -?\d+(\.\d+)?, )(-?\d+(\.\d+)?), (-?\d+(\.\d+)?)\)/;
 
-export const getTransformFromCss = (transformCssString: string) => {
+export type Point2D = {
+  x: number;
+  y: number;
+};
+
+export const getTransformFromCss = (transformCssString: string): Point2D => {
   const data = transformCssString.match(transformRegexp);
   if (!data) return { x: 0, y: 0 };
   return {
@@ -27,20 +32,21 @@ export const getTransformFromCss = (transformCssString: string) => {
   };
 };
 
-export const getColorContrastValue = ([red, green, blue]: [number, number, number]) =>
+export type RGB = [number, number, number];
+export const getColorContrastValue = ([red, green, blue]: RGB) =>
   // http://www.w3.org/TR/AERT#color-contrast
   Math.round((red * 299 + green * 587 + blue * 114) / 1000);
 
-export const getContrastType = (contrastValue: number) => (contrastValue > 125 ? 'black' : 'white');
+export const getContrastType = (contrastValue: number): 'black' | 'white' => (contrastValue > 125 ? 'black' : 'white');
 
 export const shortColorRegExp = /^#[0-9a-f]{3}$/i;
 export const longColorRegExp = /^#[0-9a-f]{6}$/i;
 
-export const checkColor = (color: string) => {
+export const checkColor = (color: string): void | never => {
   if (!longColorRegExp.test(color) && !shortColorRegExp.test(color)) throw new Error(`invalid hex color: ${color}`);
 };
 
-export const hex2rgb = (color: string) => {
+export const hex2rgb = (color: string): RGB => {
   checkColor(color);
   if (shortColorRegExp.test(color)) {
     const red = parseInt(color.substring(1, 2), 16);
@@ -54,9 +60,13 @@ export const hex2rgb = (color: string) => {
   return [red, green, blue];
 };
 
-export const getNumberedArray = (arr: number[]) => arr.map((value, number) => ({ value, number }));
-export const toStringArray = (arr: { value: number; number: number }[]) =>
-  arr.map(({ value, number }) => `${value}_${number}`);
+export type NumberedArray<T> = { value: T; number: number }[];
+export function getNumberedArray<T>(arr: T[]): NumberedArray<T> {
+  return arr.map((value: T, number: number) => ({ value, number }));
+}
+export function toStringArray<T>(arr: NumberedArray<T>): string[] {
+  return arr.map(({ value, number }) => `${value}_${number}`);
+}
 
 interface Customer {
   id: string;
@@ -64,8 +74,8 @@ interface Customer {
   age: number;
   isSubscribed: boolean;
 }
-
-export const transformCustomers = (customers: Customer[]) => {
+export type CustomerMap = Record<string, Partial<Customer>>;
+export const transformCustomers = (customers: Customer[]): CustomerMap => {
   return customers.reduce((acc, customer) => {
     acc[customer.id] = { name: customer.name, age: customer.age, isSubscribed: customer.isSubscribed };
     return acc;
