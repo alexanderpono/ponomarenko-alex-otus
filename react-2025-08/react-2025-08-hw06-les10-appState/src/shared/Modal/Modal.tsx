640d92a2f2a4d3a29716fbb6e189c3cc1e57fdab
alexanderpono <brain16383@yandex.ru>
2025-12-29 23:14:00 +0300

    feat: hw06: add ModalStarter

--- react-2025-08/react-2025-08-hw06-les10-appState/src/shared/Modal/Modal.tsx
+++ react-2025-08/react-2025-08-hw06-les10-appState/src/shared/Modal/Modal.tsx
@@ -4,14 +4,20 @@ import styles from './Modal.scss';
 interface ModalProps {
     visible: boolean;
     children?: React.ReactNode;
+    handleBtCloseClick?: () => void;
 }
-export const Modal: React.FC<ModalProps> = ({ visible, children }) => {
+export const Modal: React.FC<ModalProps> = ({ visible, children, handleBtCloseClick }) => {
     return (
         <>
             {visible && (
                 <div className={styles.Modal}>
                     <div className={styles.window}>
-                        <button className={styles.closeButton} id="closeModalBtn" aria-label="Закрыть">
+                        <button
+                            className={styles.closeButton}
+                            id="closeModalBtn"
+                            aria-label="Закрыть"
+                            onClick={handleBtCloseClick}
+                        >
                             &times;
                         </button>
                         <div className={styles.content}>{children}</div>
