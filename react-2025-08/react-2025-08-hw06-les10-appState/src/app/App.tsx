cd914b13f07e20f5aa583b5ccedf82e3b7caaaa5
alexanderpono <brain16383@yandex.ru>
2025-12-30 21:33:12 +0300

    feat: hw06: add multilanguage support

--- react-2025-08/react-2025-08-hw06-les10-appState/src/app/App.tsx
+++ react-2025-08/react-2025-08-hw06-les10-appState/src/app/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './App.css';
 import Modal from 'src/shared/Modal/Modal';
 import Layout from 'src/shared/Layout/Layout';
@@ -10,26 +10,38 @@ import ProductCard from 'src/shared/ProductCard/ProductCard';
 import DetailedProductCard from 'src/shared/DetailedProductCard/DetailedProductCard';
 import CartItem from 'src/shared/CartItem/CartItem';
 import { ThemeProvider } from 'src/shared/ThemeContext/ThemeContext';
+import { I18nProvider, I18nContext } from 'src/shared/I18nContext/I18nContext';
 
 function App() {
+    const { language, i18n } = useContext(I18nContext);
+    const translations = i18n[language].example;
+
     return (
-        <ThemeProvider>
-            <Layout>
-                <BtToBacket count={0} />
-                <BtToBacket count={1} />
-                <Modal visible={false}>
-                    <h2>Заголовок</h2>
-                    <p>Текст писать ту-тут</p>
-                </Modal>
-                <ProductCard image="" count={0} price={1999} name={shortText} description={middleText} />
-                <ProductCard image="cat.jpg" count={0} price={1999} name={shortText} description={middleText} />
-                <DetailedProductCard image="cat.jpg" count={0} price={1999} name={shortText} category="Cats">
-                    {bigText}
-                </DetailedProductCard>
-                <CartItem image="" count={1} price={1999} name={shortText} />
-                <CartItem image="cat.jpg" count={2} price={1999} name={shortText} />
-            </Layout>
-        </ThemeProvider>
+        <I18nProvider>
+            <ThemeProvider>
+                <Layout>
+                    <BtToBacket count={0} />
+                    <BtToBacket count={1} />
+                    <Modal visible={false}>
+                        <h2>{translations.modalHeader}</h2>
+                        <p>{translations.modalText}</p>
+                    </Modal>
+                    <ProductCard image="" count={0} price={1999} name={shortText} description={middleText} />
+                    <ProductCard image="cat.jpg" count={0} price={1999} name={shortText} description={middleText} />
+                    <DetailedProductCard
+                        image="cat.jpg"
+                        count={0}
+                        price={1999}
+                        name={shortText}
+                        category={translations.category}
+                    >
+                        {bigText}
+                    </DetailedProductCard>
+                    <CartItem image="" count={1} price={1999} name={shortText} />
+                    <CartItem image="cat.jpg" count={2} price={1999} name={shortText} />
+                </Layout>
+            </ThemeProvider>
+        </I18nProvider>
     );
 }
 
