034ab67e3d6e893a2ac626ff4b1db5a72e6d9c24
alexanderpono <brain16383@yandex.ru>
2025-12-31 12:09:33 +0300

    feat: hw07 make copy from hw06

---
+++ react-2025-08/react-2025-08-hw07-les12-listsPortals/src/shared/DetailedProductCard/DetailedProductCard.stories.tsx
@@ -0,0 +1,74 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { DetailedProductCard, DetailedProductCardProps } from './DetailedProductCard';
+import React from 'react';
+import { bigText } from 'src/constants/bigText';
+import { shortText } from 'src/constants/shortText';
+
+const meta: Meta<typeof DetailedProductCard> = {
+    title: 'shared/DetailedProductCard',
+    component: DetailedProductCard,
+    tags: ['autodocs'],
+    parameters: {
+        layout: 'fullscreen'
+    },
+    argTypes: {
+        image: {
+            control: { type: 'select' },
+            options: ['', 'cat.jpg']
+        }
+    }
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+const defaultStory: Story = {
+    render: (args: DetailedProductCardProps) => {
+        return (
+            <DetailedProductCard
+                image={args.image}
+                count={args.count}
+                price={args.price}
+                name={args.name}
+                category={args.category}
+            >
+                {args.children}
+            </DetailedProductCard>
+        );
+    }
+};
+
+export const NoImage: Story = {
+    ...defaultStory,
+    args: {
+        image: '',
+        count: 0,
+        price: 1999,
+        name: shortText,
+        children: [bigText, <h3>Additional info</h3>, bigText],
+        category: shortText
+    }
+};
+
+export const NoImage1: Story = {
+    ...defaultStory,
+    args: {
+        image: '',
+        count: 1,
+        price: 1999,
+        name: shortText,
+        children: [bigText, <h3>Additional info</h3>, bigText],
+        category: shortText
+    }
+};
+
+export const Cat1: Story = {
+    ...defaultStory,
+    args: {
+        image: 'cat.jpg',
+        count: 1,
+        price: 1999,
+        name: shortText,
+        children: [bigText, <h3>Additional info</h3>, bigText],
+        category: shortText
+    }
+};
