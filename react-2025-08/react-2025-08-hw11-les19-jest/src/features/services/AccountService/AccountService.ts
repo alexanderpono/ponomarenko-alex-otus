b3ca00687d8f76fba4190f25a9ca5c4c58e01dad
alexanderpono <brain16383@yandex.ru>
2026-02-12 00:06:41 +0300

    feat: hw11: add user type discount logic

--- react-2025-08/react-2025-08-hw11-les19-jest/src/features/services/AccountService/AccountService.ts
+++ react-2025-08/react-2025-08-hw11-les19-jest/src/features/services/AccountService/AccountService.ts
@@ -2,16 +2,18 @@ import { Product } from 'src/entities/Product';
 import { User } from 'src/entities/User';
 import { IAccountService } from './AccountService.types';
 import { IProductDiscountService } from 'src/features/services/ProductDiscountService/ProductDiscountService.types';
+import { IUserDiscountService } from 'src/features/services/UserDiscountService/UserDiscountService.types';
 
 export class AccountService implements IAccountService {
-    constructor(private productDiscounts: IProductDiscountService) {}
+    constructor(private productDiscounts: IProductDiscountService, private usersDiscounts: IUserDiscountService) {}
 
     getDiscount = (user: User, product: Product) => {
-        return (
+        const price =
             product.price *
             (1 -
                 user.personalDiscountPercents / 100 -
-                this.productDiscounts.getProductDiscountPercent(product.type) / 100)
-        );
+                this.usersDiscounts.getUserTypeDiscountPercent(user.type) / 100 -
+                this.productDiscounts.getProductDiscountPercent(product.type) / 100);
+        return price > 0 ? price : 0;
     };
 }
