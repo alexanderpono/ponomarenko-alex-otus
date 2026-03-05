df58f357e991ea2c4f75a5c34eebc9709ccb0392
alexanderpono <brain16383@yandex.ru>
2026-03-05 23:59:10 +0300

    feat: hw16 - autoselect http / https for API calls

--- react-2025-08/react-2025-08-hw16-shop/src/features/services/AuthAPI/AuthAPI.ts
+++ react-2025-08/react-2025-08-hw16-shop/src/features/services/AuthAPI/AuthAPI.ts
@@ -1,10 +1,12 @@
-import { API_URL, COMMAND_ID } from 'src/constants/config';
+import { COMMAND_ID } from 'src/constants/config';
 import { AuthResult, LoginParams, RegisterParams } from './AuthAPI.types';
 import { CONTENT_JSON } from 'src/constants/API';
 
 export class AuthAPI {
+    constructor(private apiUrl: string) {}
+
     register = (params: RegisterParams): Promise<AuthResult> => {
-        return fetch(API_URL + '/signup', {
+        return fetch(this.apiUrl + '/signup', {
             method: 'POST',
             headers: {
                 'Content-type': CONTENT_JSON
@@ -22,7 +24,7 @@ export class AuthAPI {
     };
 
     login = (params: LoginParams): Promise<AuthResult> => {
-        return fetch(API_URL + '/signin', {
+        return fetch(this.apiUrl + '/signin', {
             method: 'POST',
             headers: {
                 'Content-type': CONTENT_JSON
--- react-2025-08/react-2025-08-hw16-shop/src/features/services/ProductAPI/ProductAPI.ts
+++ react-2025-08/react-2025-08-hw16-shop/src/features/services/ProductAPI/ProductAPI.ts
@@ -1,10 +1,9 @@
-import { API_URL } from 'src/constants/config';
 import { CONTENT_JSON } from 'src/constants/API';
 import { GetProductsAnswer } from './ProductAPI.types';
 import { Product } from 'src/entities/Product';
 
 export class ProductAPI {
-    constructor(private token: string) {}
+    constructor(private apiUrl: string, private token: string) {}
 
     getHeaders = () => {
         const headers: HeadersInit = {
@@ -17,7 +16,7 @@ export class ProductAPI {
     };
 
     getProducts = (): Promise<GetProductsAnswer> => {
-        return fetch(API_URL + '/products', { headers: this.getHeaders() }).then((response: Response) => {
+        return fetch(this.apiUrl + '/products', { headers: this.getHeaders() }).then((response: Response) => {
             return response.json().then((result) => {
                 if (response.status !== 200) {
                     return Promise.reject(result);
@@ -32,7 +31,7 @@ export class ProductAPI {
         const { ...restToSave } = productToSave;
         delete restToSave['id'];
 
-        return fetch(API_URL + '/products', {
+        return fetch(this.apiUrl + '/products', {
             method: 'POST',
             headers: this.getHeaders(),
             body: JSON.stringify(restToSave)
@@ -48,7 +47,7 @@ export class ProductAPI {
     };
 
     updateProduct = (productToSave: Product): Promise<unknown> => {
-        return fetch(API_URL + '/products/' + productToSave.id, {
+        return fetch(this.apiUrl + '/products/' + productToSave.id, {
             method: 'PUT',
             headers: this.getHeaders(),
             body: JSON.stringify(productToSave)
